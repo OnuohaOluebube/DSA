8af68823c405e6a5af312747d04c36117199f292
Oluebube <62969791+OnuohaOluebube@users.noreply.github.com>
2022-09-01 05:34:12 -0700

    Time: 99 ms (30.34%), Space: 41.8 MB (90.18%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -8,10 +8,11 @@ var searchInsert = function(nums, target) {
    let j = nums.length - 1
     while(i <= j) {
           const mid = Math.floor((i + j)/2)
-          if(target > nums[mid]){
-         i = mid + 1
+          if (target == nums[mid]) return mid
+          if(target < nums[mid]){
+         j = mid - 1
     } else { 
-        j = mid - 1
+        i = mid + 1
     }
     }
         return i
