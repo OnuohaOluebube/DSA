41ae6fec511929974eb566fd0eefb31a0d9e9c3c
Oluebube <62969791+OnuohaOluebube@users.noreply.github.com>
2022-09-02 07:31:50 -0700

    Time: 111 ms (12.34%), Space: 42.6 MB (25.23%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -27,15 +27,3 @@ var isSameTree = function(p, q) {
    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
    }
 
-//     if (p === q) return true;
-//     if (p === null && q !== null) {
-//         return false;
-//     };
-//     if (p !== null && q === null) {
-//         return false;
-//     };
-//     if (p.val !== q.val) {
-//         return false;
-//     };
-//     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-// }
