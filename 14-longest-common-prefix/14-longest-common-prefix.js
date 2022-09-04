36f6fd33a7f7e1cf2e35016a22fe5e6c8985e462
Oluebube <62969791+OnuohaOluebube@users.noreply.github.com>
2022-09-04 00:40:25 -0700

    Time: 89 ms (62.52%), Space: 42.2 MB (76.76%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -5,12 +5,12 @@
 var longestCommonPrefix = function(strs) {
 
 
-  let r = '';
+  let result = '';
   for (let i = 0; i < strs[0].length; i++) {
     for (let j = 1; j < strs.length; j++) {
-      if (strs[0][i] !== strs[j][i]) return r;
+      if (strs[0][i] !== strs[j][i]) return result;
     }
-    r += strs[0][i];
+    result += strs[0][i];
   }
-  return r;
+  return result;
 };
