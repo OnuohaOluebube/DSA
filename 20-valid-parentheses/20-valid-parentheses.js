c79c1a4c414972aa73d2539fc793ece57e189922
Oluebube <62969791+OnuohaOluebube@users.noreply.github.com>
2022-09-04 13:00:38 -0700

    Time: 117 ms (21.92%), Space: 42.5 MB (61.99%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,20 +3,71 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-      let map = {
-        ")": "(",
-        "]": "[",
-        "}": "{"
-      }
-let arr = []
-for(let i = 0; i < s.length; i++){
-    if(s[i] === "(" || s[i] === "[" ||s[i] === "{"  ){
-        arr.push(s[i])
-    }else{
-        if(arr[arr.length - 1] === map[s[i]]){
-            arr.pop()
-        }else{return false}
-    }
+let map = {
+    ")":"(",
+     "}":"{",
+     "]":"[",
 }
-    return arr.length === 0 ? true:false
+
+let stack = []
+
+for (let i = 0; i<s.length; i++){
+    if(s[i] === "(" || s[i] === "{" || s[i] === "[" ){
+        stack.push(s[i])
+    } else if (stack[stack.length -1] === map[s[i]]){
+         stack.pop(s[i])
+    } else {
+        return false
+    }
+    
+  
+        
+}
+     return stack.length === 0 ?  true : false
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//       let map = {
+//         ")": "(",
+//         "]": "[",
+//         "}": "{"
+//       }
+// let arr = []
+// for(let i = 0; i < s.length; i++){
+//     if(s[i] === "(" || s[i] === "[" ||s[i] === "{"  ){
+//         arr.push(s[i])
+//     }else{
+//         if(arr[arr.length - 1] === map[s[i]]){
+//             arr.pop()
+//         }else{return false}
+//     }
+// }
+//     return arr.length === 0 ? true:false
 };
+
+
