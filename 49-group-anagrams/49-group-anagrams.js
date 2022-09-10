c4f0d4dc2bfafa00a4764a8d1ae774c4aaa8b9dd
Oluebube <62969791+OnuohaOluebube@users.noreply.github.com>
2022-09-10 16:33:06 +0100

    Time: 224 ms (37.35%), Space: 55.1 MB (18.85%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+    const CODES = {
+        "a":0,
+        "b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9,"k":10,"l":11,"m":12,"n":13,"o":14,"p":15,"q":16,"r":17,"s":18,"t":19,"u":20,"v":21,"w":22,"x":23,"y":24,"z":25,
+    }
+ var groupAnagrams = function(strs) {
+    
+
+const map = Object.create(null)
+  for(let str of strs){
+const hash = hashWord(str)
+if(!(hash in map)){
+    map[hash] = []
+}
+   
+          map[hash].push(str)
+    }    
+      
+      const groups = []
+      for(let key in map){
+groups.push(map[key])
+      
+  
+  }
+     
+     return groups
+ }    
+   function hashWord(str){
+    const hash = new Array(26).fill(0)
+    
+    for(let ch of str){
+        ++hash[CODES[ch]]
+    } 
+       
+       return hash.toString()
+};
+
+
+  
+ 
+
+
