fdf0d11a8352c44fb57c8eaddae7cfa95397ca94
Oluebube <62969791+OnuohaOluebube@users.noreply.github.com>
2022-09-24 15:45:00 +0100

    Time: 2272 ms (5.57%), Space: 22.6 MB (53.22%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        res = 0 
+        l = 0
+        for r in range(1, len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+            res = max(res, prices[r] - prices[l])
+                      
+        return res
+        
+        
