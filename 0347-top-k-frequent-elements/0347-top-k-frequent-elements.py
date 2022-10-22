6cc5b7084e8954e84d6c86f21d025d1f9b03465f
Oluebube <62969791+OnuohaOluebube@users.noreply.github.com>
2022-10-22 05:47:29 +0100

    Time: 284 ms (12.59%), Space: 21.1 MB (6.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,23 +6,22 @@ class Solution(object):
         :rtype: List[int]
         """
         count = {}
-        arr =[[] for num in range(len(nums) +1)]
-        print(arr)
-        ans= []
+        arr = [[] for num in range(len(nums) +1)]
+        res = []
         
         for num in nums:
-            count[num] = 1 + count.get(num, 0)
-        
-        for i in count:
-            arr[count[i]].append(i)
+            count[num] = 1+count.get(num, 0)
+        for i,c in count.items():
+            arr[c].append(i)
+        for a in range(len(arr) -1, 0, -1):
+            for i in arr[a]:
+                res.append(i)
+                if len(res) == k:
+                    return res
             
-        for i in range(len(arr) -1,0,-1):
-            for a in arr[i]:
-           
-                ans.append(a)
-                if len(ans) == k:
+            
 
-                    return ans
+
             
          
             
